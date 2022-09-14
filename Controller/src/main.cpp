82f8476c4ea478dda243538dac8c4fa733a45fc6
Timboooh <th.vansark@student.avans.nl>
2022-09-14 22:23:09 +0200

    Basic functionality

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,146 +1,180 @@
-#include "main.hpp"
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Adafruit_BME680.h>
 
-#define RED_PIN 19
-#define GREEN_PIN 21
-#define BLUE_PIN 5
+#include "Credentials.h"
 
-const char *mqtt_server = "mqtt.timb.one";
+void wifi_setup();
+void wifi_update();
+void mqtt_setup();
+void mqtt_update();
+void bme680_setup();
+void bme680_update();
+void mqtt_receive(const char *topic, byte *message, unsigned int length);
+void mqtt_publish();
 
+DynamicJsonDocument jsonDocument(1024);
 WiFiClient wifiClient;
-PubSubClient client(wifiClient);
+PubSubClient mqttClient(wifiClient);
 
-long lastMsg = 0;
-int sensorValue;
+Adafruit_BME680 bme;
+
+long lastUpdate = 0;
+
+void wifi_setup()
+{
+    Serial.print(F("Connecting to "));
+    Serial.print(WIFI_SSID);
+
+    WiFi.begin(WIFI_SSID, WIFI_PW);
+
+    bool led_status = false;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+        
+        led_status = !led_status;
+        digitalWrite(LED_BUILTIN, led_status);
+    }
+    digitalWrite(LED_BUILTIN, false);
+    Serial.println("");
+
+    Serial.println(F("WiFi connected"));
+    Serial.print(F("IP address: "));
+    Serial.println(WiFi.localIP());
+}
+
+void wifi_update()
+{
+}
+
+void mqtt_setup()
+{
+    mqttClient.setServer(MQTT_SERVER, 1883);
+    mqttClient.setCallback(mqtt_receive);
+}
+
+void mqtt_update()
+{
+    if (!mqttClient.connected())
+    {
+        // Reconnect
+        // Loop until we're reconnected
+        while (!mqttClient.connected())
+        {
+            Serial.print("Attempting MQTT connection...");
+            // Attempt to connect
+            if (mqttClient.connect("ESP32", MQTT_USER, MQTT_PW))
+            {
+                Serial.println("connected");
+                // Subscribe
+                mqttClient.subscribe(INCOMING_TOPIC);
+            }
+            else
+            {
+                Serial.print("failed, rc=");
+                Serial.print(mqttClient.state());
+                Serial.println(" try again in 5 seconds");
+                // Wait 5 seconds before retrying
+                bool led_status = false;
+                for (size_t i = 0; i < 25; i++)
+                {
+                    delay(200);
+                    
+                    led_status = !led_status;
+                    digitalWrite(LED_BUILTIN, led_status);
+                }
+                digitalWrite(LED_BUILTIN, false);
+                
+            }
+        }
+    }
+
+    mqttClient.loop();
+}
+
+void bme680_setup()
+{
+    if (!bme.begin())
+        Serial.println(F("Error connecting BME680 Sensor"));
+
+    // Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+void bme680_update()
+{
+    if (!bme.performReading())
+    {
+        Serial.println("Failed to read BME680 Sensor");
+        return;
+    }
+
+    jsonDocument["Temperature"] = bme.temperature;
+    jsonDocument["Pressure"] = bme.pressure / 100.0;
+    jsonDocument["Humidity"] = bme.humidity;
+    jsonDocument["Gas"] = bme.gas_resistance / 1000.0;
+}
 
 void setup()
 {
-  pinMode(BLUE_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(RED_PIN, OUTPUT);
+    // Communication setup
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    wifi_setup();
+    mqtt_setup();
 
-  digitalWrite(BLUE_PIN, false);
-  digitalWrite(GREEN_PIN, false);
-  digitalWrite(RED_PIN, false);
-
-  Serial.begin(115200);
-  setup_wifi();
-  setup_mqtt();
+    // Sensor setup
+    bme680_setup();
 }
 
 void loop()
 {
-  // Serial.print("Value: ");
-  // Serial.println(analogRead(34));
-  if (!client.connected())
-  {
-    // reconnect_mqtt();
-  }
-  client.loop();
+    wifi_update();
+    mqtt_update();
 
-  long now = millis();
-  if (now - lastMsg > 500)
-  {
-    lastMsg = now;
+    long now = millis();
+    if (now - lastUpdate > 5000)
+    {
+        lastUpdate = now;
 
-    sensorValue = analogRead(34);
+        bme680_update();
 
-    char valueString[8];
-    dtostrf(sensorValue, 1, 2, valueString);
-    Serial.print("Value: ");
-    Serial.println(valueString);
-
-    client.publish("esp32/testSensor", valueString);
-  }
-
-  // delay(1000);
+        mqtt_publish();
+    }
 }
 
-void setup_wifi()
+void mqtt_publish()
 {
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
+    char messageBuffer[256];
+    size_t length = serializeJsonPretty(jsonDocument, messageBuffer);
 
-  WiFi.begin(WIFI_SSID, WIFI_PW);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    mqttClient.publish(OUTGOING_TOPIC, messageBuffer, length);
 }
 
-void setup_mqtt()
+void mqtt_receive(const char *topic, byte *message, unsigned int length)
 {
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback_mqtt);
-  while (!client.connected())
-  {
-    if (client.connect("ESP32", MQTT_USER, MQTT_PW))
-    {
-      client.subscribe("esp32/testColor/#");
-    }
-  }
-}
+    Serial.print(F("Message received: "));
+    Serial.println(topic);
+    Serial.print(F("Message: "));
 
-void callback_mqtt(const char *topic, byte *message, unsigned int length)
-{
-  Serial.print("Message received: ");
-  Serial.println(topic);
-  Serial.print("Message: ");
+    String messageTemp;
 
-  String messageTemp;
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+    Serial.println();
 
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-
-  if (String(topic) == "esp32/testColor/R")
-  {
-    if (messageTemp == "on")
+    if (String(topic) == "esp32/testColor/R")
     {
-      Serial.println("on");
-      digitalWrite(RED_PIN, HIGH);
     }
-    else if (messageTemp == "off")
-    {
-      Serial.println("off");
-      digitalWrite(RED_PIN, LOW);
-    }
-  }
-
-  if (String(topic) == "esp32/testColor/G")
-  {
-    if (messageTemp == "on")
-    {
-      Serial.println("on");
-      digitalWrite(GREEN_PIN, HIGH);
-    }
-    else if (messageTemp == "off")
-    {
-      Serial.println("off");
-      digitalWrite(GREEN_PIN, LOW);
-    }
-  }
-
-  if (String(topic) == "esp32/testColor/B")
-  {
-    if (messageTemp == "on")
-    {
-      Serial.println("on");
-      digitalWrite(BLUE_PIN, HIGH);
-    }
-    else if (messageTemp == "off")
-    {
-      Serial.println("off");
-      digitalWrite(BLUE_PIN, LOW);
-    }
-  }
 }
--- Controller/src/main.hpp
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include "Credentials.h"
-
-void setup_wifi();
-void setup_mqtt();
-void callback_mqtt(const char* topic, byte* message, unsigned int length);
